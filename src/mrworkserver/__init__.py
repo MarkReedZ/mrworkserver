070ba4a795412e3ca8f91b3a41121b9e7fdc10fe
MarkReedZ <mark@booncapital.com>
2024-03-21 18:12:24 +0000

    Remove lrudict

--- src/mrworkserver/__init__.py
+++ src/mrworkserver/__init__.py
@@ -3,7 +3,7 @@ from .statserver import setup_statserver, stats_timer
 from .internals import Protocol
 from .internals import WorkServer as CWorkServer
 from .workserver import *
-from .lrudict import LruDict
+#from .lrudict import LruDict
 
 __version__=0.12
 
